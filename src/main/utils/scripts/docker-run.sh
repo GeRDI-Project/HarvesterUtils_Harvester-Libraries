b0437dcefdc82ea166c3110bf496ec1af45f4b1d
Robin Weiß <row@informatik.uni-kiel.de>
2018-01-05 09:58:59 +0100

    SAI-345 The access rights of war files that are included in a docker image are now set to read-write to allow certain docker versions to be able to run the jetty container. Additionally got rid of the getImageName script and added the logic to the docker-build script which is used instead, removing redundant code in the other scripts.

--- src/main/utils/scripts/docker-run.sh
+++ src/main/utils/scripts/docker-run.sh
@@ -3,7 +3,6 @@
 # The name of the image is derived from the war-file's pom.properties file, whereas the image version can be explicitly defined.
 #
 # Arguments:
-#
 # 1 - docker image name
 #     If "", the name will be the war file name up until the first character that is neither a letter nor a -
 #     If "<maven>", the name will be the artifactId defined in the pom.properties of the war-file.
@@ -14,19 +13,17 @@
 #     If "<maven>", the tag will be the one defined in the pom.properties of the war-file.
 #     If "<git>", the tag will be the current git tag.
 
+
 # navigate to project root directory
 projectRoot=$(git rev-parse --show-toplevel)
 if [ "$projectRoot" != "" ]; then
   cd $projectRoot
 fi
 
-image=$(./scripts/docker-getImageName.sh "$1" "$2" "")
+# build image
+fullImageName=$(./scripts/docker-build.sh "$1" "$2" "")
 
-if [ "$image" != "" ]; then 
-  # build image
-  echo "Building docker image $image"
-  docker build -t $image .
-  
-  # run image
-  docker run -p 8080:8080 $image
+# run image if it exists
+if [ "$fullImageName" != "" ]; then 
+  docker run -p 8080:8080 $fullImageName
 fi
