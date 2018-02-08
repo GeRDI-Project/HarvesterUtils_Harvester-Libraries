cbc728958f84a0f37de418a302d835451337c1d4
Robin Weiß <row@informatik.uni-kiel.de>
2018-02-08 12:25:11 +0100

    SAI-340 Reworked the logic of AStyle scripts to only format a specified list of file formats. This fixes an issue where JSON files are formatted to become ugly. Also added headers.

--- src/main/utils/scripts/formatting/astyle-check.sh
+++ src/main/utils/scripts/formatting/astyle-check.sh
@@ -1,3 +1,33 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# This Script checks all source files that match the extensions defined in astyle-fileTypes.ini
+# and prints a list of possibly unformatted files.
+
+# abort if AStyle is not installed
+isInstalled=$(command -v astyle)
+
+if [ "$isInstalled" = "" ]; then
+  echo "Cannot format: AStyle 3.11 is not installed!"
+  exit 1
+fi
+
 echo "Checking Code Formatting:"
 
 # navigate to project root directory
@@ -6,38 +36,44 @@ if [ "$projectRoot" != "" ]; then
   cd $projectRoot
 fi
 
-sourcePath="$projectRoot/src/*"
+# read ini files
 formattingStyle="$projectRoot/scripts/formatting/astyle-kr.ini"
+includedFiles=$(cat "$projectRoot/scripts/formatting/astyle-includedFiles.ini")
 
 # run AStyle without changing the files
-result=$(astyle --options="$formattingStyle" --dry-run --recursive --formatted "$sourcePath")
-returnCode=$?
+CountAndPrintUnformattedFiles() {
+  errorCount=0
+  unformattedLog='
+Unformatted Files:
+'
+  printf '%s\n' "$includedFiles" | ( while IFS= read -r fileType
+  do 
+    result=$(astyle "$projectRoot/src/*.$fileType" --options="$formattingStyle" --dry-run --recursive --formatted )
+	
+	# extract relevant lines
+	result=$(echo $result | grep -oP 'main/.+?'$fileType)
+	
+	# generate log of unformatted files
+	unformattedLog=$unformattedLog$result
+	
+	# count unformatted files
+	if [ "$result" != "" ]; then
+      errorCount=$(expr $errorCount + $(echo ''"$result"'' | wc -l))
+	fi
+  done
+  
+  if [ $errorCount -ne 0 ]; then
+    echo ''"$unformattedLog"'' >&2
+  fi
+  echo $errorCount
+  )
+}
 
-# remove all text up until the name of the first unformatted file
-newResult=${result#*Formatted  }
-
-errorCount=0
-
-while [ "$newResult" != "$result" ]
-do
-errorCount=$(($errorCount + 1))
-result="$newResult"
-
-# retrieve the name of the unformatted file
-fileName=$(echo $result | sed -e "s/Formatted .*//gi")
- 
-# log the unformatted file
-echo "Unformatted File: $fileName"
-
-# remove all text up until the name of the next unformatted file
-newResult=${result#*Formatted  }
-done
-
+errorCount=$(CountAndPrintUnformattedFiles)
 if [ $errorCount -ne 0 ]; then
-  echo "\\nFound $errorCount unformatted files! Please use the ArtisticStyle formatter before committing your code!\\n(see https://wiki.gerdi-project.de/display/GeRDI/%5BWIP%5D+How+to+Format+Code)"
-  exit 1
-elif [ $returnCode -ne 0 ]; then
-  echo "Astyle returned an error. Please make sure that AStyle is installed!"
+  echo '
+Found '"$errorCount"' unformatted files! Please use the ArtisticStyle formatter before committing your code!
+(see https://wiki.gerdi-project.de/display/GeRDI/%5BWIP%5D+How+to+Format+Code)'
   exit 1
 else
   echo "All files are properly formatted!"
--- src/main/utils/scripts/formatting/astyle-format.sh
+++ src/main/utils/scripts/formatting/astyle-format.sh
@@ -1,3 +1,32 @@
+#!/bin/bash
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# This Script formats all source files that match the extensions defined in astyle-fileTypes.ini
+
+# abort if AStyle is not installed
+isInstalled=$(command -v astyle)
+
+if [ "$isInstalled" = "" ]; then
+  echo "Cannot format: AStyle 3.11 is not installed!"
+  exit 1
+fi
+
 echo "Formatting Code:"
 
 # navigate to project root directory
@@ -6,16 +35,12 @@ if [ "$projectRoot" != "" ]; then
   cd $projectRoot
 fi
 
-sourcePath="$projectRoot/src/*"
+# read ini files
 formattingStyle="$projectRoot/scripts/formatting/astyle-kr.ini"
+includedFiles=$(cat "$projectRoot/scripts/formatting/astyle-includedFiles.ini")
 
-# run AStyle without changing the files
-astyle --options="$formattingStyle" --recursive --suffix=none --formatted "$sourcePath"
-returnCode=$?
-
-if [ $returnCode -ne 0 ]; then
-  echo "Astyle returned an error. Please make sure that AStyle is installed!"
-  exit 1
-else
-  exit 0
-fi
+# run AStyle for all included filetypes
+printf '%s\n' "$includedFiles" | while IFS= read -r fileType
+do 
+  astyle "$projectRoot/src/*.$fileType" --options="$formattingStyle" --recursive --suffix=none --formatted
+done
