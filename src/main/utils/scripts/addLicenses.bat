b4c49fea3ac0f46dfb91fb272eea4cd92e884cfe
Robin Weiß <row@informatik.uni-kiel.de>
2018-03-06 13:46:14 +0100

    SAI-723 Created a batch script for creating headers

---
+++ src/main/utils/scripts/addLicenses.bat
@@ -0,0 +1,153 @@
+:: Copyright © 2018 Robin Weiss (http://www.gerdi-project.de)
+::
+:: Licensed under the Apache License, Version 2.0 (the "License");
+:: you may not use this file except in compliance with the License.
+:: You may obtain a copy of the License at
+::
+::   http://www.apache.org/licenses/LICENSE-2.0
+::
+:: Unless required by applicable law or agreed to in writing,
+:: software distributed under the License is distributed on an
+:: "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+:: KIND, either express or implied.  See the License for the
+:: specific language governing permissions and limitations
+:: under the License.
+
+:: Description:
+:: This script extracts the owner property and inception year from the pom.xml and
+:: creates headers in source files using this info. If the inception year property
+:: is missing from the pom.xml, the last modified date of the pom.xml is retrieved
+:: and added to the pom.xml.
+:: if no owner property was specified, the developer names are used instead.
+::
+:: Arguments: none
+
+@ECHO OFF
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+SET pomPath=pom.xml
+IF NOT EXIST "%pomPath%" (
+  echo Cannot set license headers, because no pom.xml exists^^!
+  ENDLOCAL
+  ECHO ON
+  EXIT /B 1
+)
+
+:: retrieve inception year
+for /f "tokens=1,2,3 delims=><" %%a in ('type !pomPath!^|find "<inceptionYear>"') do (
+  SET inceptionYear=%%c
+)
+
+IF "%inceptionYear%" == "" (
+  echo No ^<inceptionYear^> tag was found, using the last modified date from the pom.xml
+  
+  :: get file creation date
+  CALL :getFileCreationDate "!pomPath!" "inceptionYear"
+  
+  echo Adding ^<inceptionYear^> tag to pom.xml
+  
+  :: try to add year after descriptionTag
+  SET insertionTag=modelVersion
+  for /f "delims=" %%a in ('type !pomPath!^|find "</description>"') do (
+    SET insertionTag=description
+  )
+  
+  :: retrieve whitespace that comes before the opening tag
+  for /f "tokens=1,2 delims=<" %%a in ('type !pomPath!^|find "</%%insertionTag%%>"') do (
+    SET whitespace=%%a
+  )
+  
+  :: create temporary pom.xml
+  copy /y NUL tempPom.xml >NUL
+  
+  :: write content to temporary pom.xml
+  for /f "tokens=* delims=" %%a in (!pomPath!) do (
+	:: disable delayed expansion for treating special characters
+    SETLOCAL DISABLEDELAYEDEXPANSION
+	rem copy line to temporary file
+	echo.%%a>> tempPom.xml
+    ENDLOCAL
+	
+	:: remove double quotes from line, because they will break the subsequent text replacement
+	SET line=%%a
+	SET line=!line:"=!
+	
+	:: remove the insertion closing tag from the line, if it exists
+	CALL SET "lineWithoutInsertTag=%%line:^</!insertionTag!^>=%%"
+	
+	:: check if the current line has the insertion closing tag
+	IF NOT "!lineWithoutInsertTag!" == "!line!" (
+	  echo.!whitespace!^<inceptionYear^>!inceptionYear!^</inceptionYear^>>> tempPom.xml
+	)
+  )
+  :: replace pom.xml
+  xcopy tempPom.xml /y /u /a !pomPath!
+  del tempPom.xml
+)
+
+:: retrieve owner tag value
+for /f "tokens=1,2,3 delims=><" %%a in ('type !pomPath!^|find "<owner>"') do (
+  SET owner=%%c
+)
+
+:: retrieve developer names if no owner was specified
+IF "%owner%" == "" (
+  :: retrieve line number of <developer> tag
+  for /f "tokens=1,2,3 delims=[]" %%a in ('type !pomPath!^|find /N "<developers>"') do (
+    SET /a developersOpeningLine=%%a
+  )
+  
+  IF NOT "!developersOpeningLine!" == "" (
+    :: retrieve line number of </developer> tag
+    for /f "tokens=1,2,3 delims=[]" %%a in ('type !pomPath!^|find /N "</developers>"') do (
+      SET /a developersClosingLine=%%a
+    )
+
+	:: concatenate developer names
+    for /f "tokens=1,2,3,4 delims=[]<>" %%a in ('type !pomPath!^|find /N "<name>"') do (
+      SET /a lineNumber=%%a
+	  SET devName=%%d
+	
+	  IF !lineNumber! GTR !developersOpeningLine! (
+	    IF !lineNumber! LSS !developersClosingLine! (
+          IF "!owner!" == "" (
+            SET owner=!devName!
+          ) ELSE (
+            SET owner=!owner!, !devName!
+          )
+        )
+      )
+    )
+  )
+)
+
+:: abort if no owner was found
+IF "%owner%" == "" (
+  echo Cannot set license headers, because no owners are specified^^!
+  ENDLOCAL
+  ECHO ON
+  EXIT /B 1
+)
+
+echo Adding %inceptionYear% Headers for owner(s): %owner%
+
+:: generate headers
+mvn generate-resources -DaddHeaders "-Downer=%owner%"
+
+:: exit script
+ENDLOCAL
+ECHO ON
+EXIT /B 0
+
+
+:: retrieves the date at which a file was created
+:getFileCreationDate
+SET fileName=%~1
+SET varName=%~2
+for /f "skip=5 tokens=1,2,3 delims=. " %%a in ('dir /a-d /tc "%fileName%"') do (
+    CALL SET "%%varName%%=%%c"
+    GOTO :loopend
+  )
+  :loopend
+EXIT /B 0
